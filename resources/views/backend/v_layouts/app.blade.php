6b31f22750736d7db131de933b527b9b98496546
faiz201 <iqbalfaizramadhan5@gmail.com>
2025-09-29 09:52:28 +0700

    ubah

--- resources/views/backend/v_layouts/app.blade.php
+++ resources/views/backend/v_layouts/app.blade.php
@@ -94,6 +94,12 @@
                                 <i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Beranda</span>
                             </a>
                         </li>
+                        <li class="nav-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('dashboard') }}"
+                                aria-expanded="false">
+                                <i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span>
+                            </a>
+                        </li>
                         <li class="sidebar-item">
                             <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('backend.user.index') }}"
                                 aria-expanded="false">
